736101737c2f7ade8a9b663d2cb2376e2f233aec
Chi-I Huang <jasson15@users.noreply.github.com>
2016-10-02 23:03:41 -0700

    Create algo-0048.cpp

---
+++ algo-0048.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int len = matrix.size();
+        for(int i = 0; i < ceil((float)len / 2); i++){
+            for(int j = 0; j < len / 2; j++){
+                auto temp = matrix[i][j];
+                matrix[i][j] = matrix[len - 1 - j][i];
+                matrix[len - 1 - j][i] = matrix[len - 1 - i][len - 1 - j];
+                matrix[len - 1 - i][len - 1 - j] = matrix[j][len - 1 - i];
+                matrix[j][len - 1 - i] = temp;
+            }
+        }
+    }
+};
