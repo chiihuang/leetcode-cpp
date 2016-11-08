56b4d6523bcab8a71fdd39cae6d45deae12661ec
Chi-I Huang <jasson15@users.noreply.github.com>
2016-11-07 18:10:00 -0800

    Update algo-0057.cpp
    better

--- algo-0057.cpp
+++ algo-0057.cpp
@@ -12,42 +12,49 @@ public:
     vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
        int len = intervals.size(), hi = len, lo = 0;
        
-       // find start
-       while(lo < hi){
-           int mid = lo + (hi - lo) / 2;
-           if (intervals[mid].end >= newInterval.start){
-               hi = mid;
-           } else {
-               lo = mid + 1;
-           }
-       }
-       
-       int s = lo;
-       hi = len - 1;
-       lo = -1;
-       while(lo < hi){
-           int mid = lo + (hi - lo + 1) / 2;
-           if (intervals[mid].start <= newInterval.end){
-               lo = mid; // todo
-           } else {
-               hi = mid - 1;
-           }
-       }
-       int e = lo;
+       int s = findStart(intervals, newInterval);
+       int e = findEnd(intervals, newInterval);
        
        // pack up
        vector<Interval> ret;
-       for(int i = 0; i < s; i++){
-           ret.push_back(intervals[i]);
-       }
+       for(int i = 0; i < s; i++) ret.push_back(intervals[i]);
        if (s < len)
             newInterval.start = min(newInterval.start, intervals[s].start);
        if (e >= 0)
             newInterval.end = max(newInterval.end, intervals[e].end);
        ret.push_back(newInterval);
-       for(int i = e + 1; i < len; i++){
-           ret.push_back(intervals[i]);
-       }
+       for(int i = e + 1; i < len; i++) ret.push_back(intervals[i]);
        return ret;
+    }
+private:
+    int findStart(vector<Interval>& intervals, Interval newInterval){
+        int len = intervals.size(), hi = len - 1, lo = 0;
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            int end = intervals[mid].end;
+            if (end == newInterval.start){
+                return mid;
+            } else if (end > newInterval.start){
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo; // intervals[lo] > target, argmin lo
+    }
+    int findEnd(vector<Interval>& intervals, Interval newInterval){
+        int len = intervals.size(), hi = len - 1, lo = 0;
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            int start = intervals[mid].start;
+            if (start == newInterval.end){
+                return mid;
+            } else if (start > newInterval.end){
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return hi; // intervals[hi] < target, argmax hi
     }
 };
